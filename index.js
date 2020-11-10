13585bcc88ee7b8c1305dc809cfcdbefc2287169
Harry Hochheiser <harryh@pitt.edu>
2020-11-09 22:39:48 -0500

    foo test

--- index.js
+++ index.js
@@ -2,32 +2,59 @@
 //npm init -y && npm install hapi --save
 
 
-const Hapi = require('hapi');
-const server = new Hapi.Server({port: 3000}); // tell hapi which TCP Port to "listen" on
+const Hapi = require('@hapi/hapi');
+async function start () {
+  // Create a server with a host and port
+  const server = Hapi.server({
+    host: 'localhost',
+    port: 3000,
+    debug: false, // disable Hapi debug console logging
+  })
 
-server.route({
-	method: 'GET',        // define the method this route will handle
-	path: '/{yourname*}', // this is how you capture route parameters in Hapi
-	handler: function(req, h) { // request handler method
-	    req.log('info','hello '+req.params.yourname);
-		return 'Hello ' + req.params.yourname + '!'; // reply with text.
-	}
-});
-
-server.route({
+    
+  // Add the route
+  server.route({
     method: 'GET',
-    path:  '/goodbye/{yourname*}',
-    handler: function(req,h) {
-	req.log('info','goodbye '+req.params.yourname);
-        return 'Goodbye '+req.params.yourname+'!';
-    }
-});
+    path: '/',
+    handler: async function (request, h) {
+      // request.log is HAPI standard way of logging
+      // you can also use a pino instance, which will be faster
+      request.logger.warn('In handler %s', request.path)
 
-async function startServer() {
-  await server.start() // start the Hapi server on your localhost
-  console.log('Now Visit: http://localhost:' + server.info.port + '/YOURNAME');
+      return 'hello world'
+    }
+  })
+
+ server.route({
+     method: 'GET',
+     path: '/hello/{name}',
+     handler: async function(request, h) {
+	 request.logger.info('In handler ', request.path)
+	 return 'hello'+request.params.name
+    }
+ })
+
+  await server.register({
+    plugin: require('hapi-pino'),
+    options: {
+      prettyPrint: process.env.NODE_ENV !== 'production',
+      // Redact Authorization headers, see https://getpino.io/#/docs/redaction
+      redact: ['req.headers.authorization']
+    }
+  })
+
+  // also as a decorated API
+  //server.logger.info('another way for accessing it')
+
+  // and through Hapi standard logging system
+  //server.log(['subsystem'], 'third way for accessing it')
+
+  await server.start()
+
+  return server
 }
 
-startServer();
-
-module.exports = server;
+start().catch((err) => {
+  console.log(err)
+  process.exit(1)
+})
